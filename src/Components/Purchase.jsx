e74c9569ed5180c28e3ecb1570d40ca6f92f9474
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-07-19 19:51:09 +0100

    Changes made on purchase component

--- src/Components/Purchase.jsx
+++ src/Components/Purchase.jsx
@@ -5,12 +5,12 @@ export default function Purchase() {
   return (
     <Box maxW={{md: '90%', base: '97%'}} mx={'auto'} mb={'10vh'} px={{md: 5, base: 3}} color={'black'}>
         <Box textAlign={'center'} borderBottomWidth={2} borderColor={'gray.600'} pb={5}></Box>
-        <Flex alignItems={'center'} justifyContent={'space-between'} width={'100%'} py={12} px={10} bg={'gray.100'} rounded={5} mt={10}>
-            <Box>
+        <Flex alignItems={'center'} justifyContent={'space-between'} flexWrap={'wrap'} width={'100%'} py={12} px={10} bg={'gray.100'} rounded={5} mt={10}>
+            <Box width={{base: '100%', md: '50%'}}>
                 <Heading fontWeight={500} fontSize={17}>Creative & Unique Sixteen Products</Heading>
                 <Text fontWeight={400} mt={4}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corporis amet elite author nulla.</Text>
             </Box>
-            <Box>
+            <Box mt={{base: 5}}>
                 <Button bg={'blue.500'} _hover={{bg: 'blue.400'}} color={'white'} rounded={3}>Purchase Now</Button>
             </Box>
         </Flex>
